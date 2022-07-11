449ac3b2090b7a973a12f0189ef0563e8390d674
jesonliao <jesonliao@tencent.com>
2022-07-11 09:28:06 +0800

    移除多余文件

--- source-map-test1/dist/test1.js
+++
@@ -1,9 +0,0 @@
-var btn = document.getElementById('btn');
-function add(a, b) {
-    return a + b;
-}
-function handlerClick() {
-    console.log('1+2=', add(1, 2));
-}
-btn === null || btn === void 0 ? void 0 : btn.addEventListener('click', handlerClick);
-//# sourceMappingURL=test1.js.map
